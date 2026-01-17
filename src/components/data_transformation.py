069e30fcd1875ee70252f82fbe0c464bd371bc43
Ayush Walmandre <ayushdre001@gmail.com>
2026-01-18 02:18:51 +1100

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -43,7 +43,7 @@ class DataTransformation:
             cat_pipeline = Pipeline(
                 steps = [
                     ("imputer", SimpleImputer(strategy="most_frequent")),
-                    ("one_hot_encoder", OneHotEncoder()),
+                    ("one_hot_encoder", OneHotEncoder(handle_unknown="ignore")),
                     ("scaler", StandardScaler(with_mean = False))
                 ]
             )
@@ -78,8 +78,8 @@ class DataTransformation:
             input_feature_train_df = train_df.drop(columns = [target_column_name], axis = 1)
             target_feature_train_df = train_df[target_column_name]
 
-            nput_feature_test_df = test_df.drop(columns = [target_column_name], axis = 1)
-            target_feature_test_df = train_df[target_column_name]
+            input_feature_test_df = test_df.drop(columns = [target_column_name], axis = 1)
+            target_feature_test_df = test_df[target_column_name]    # ✅ correct
 
 
             logging.info(
@@ -87,7 +87,7 @@ class DataTransformation:
             )
 
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr = preprocessing_obj.transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)   # ✅ correct
 
             
             train_arr = np.c_[
